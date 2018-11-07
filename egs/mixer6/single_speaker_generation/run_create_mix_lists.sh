e5ff298ad40fc38a1147df7beb4a852b83b623bc
Matthew Maciejewski <mmaciej2@jhu.edu>
2018-11-07 11:37:41 -0500

    updated additional mixture sets

--- egs/mixer6/single_speaker_generation/run_create_mix_lists.sh
+++ egs/mixer6/single_speaker_generation/run_create_mix_lists.sh
@@ -47,21 +47,36 @@ if [ $stage -le 2 ]; then
 ### Prepare mix lists
   mkdir -p mixes
   for ch in 02 09; do
+    # Create base filelists
     for dset in tr tt cv; do
       mkdir -p tmp_mix/$dset/CH$ch/
       awk -v ch=$ch -v dset=$dset '{print dset"/CH"ch"/"$1".wav"}' \
         data/intv_CH${ch}_${dset}/segments > tmp_mix/$dset/CH$ch/filelist.txt
     done
+
+    # Create filelist for set speaker-balanced to WSJ0
+    mkdir -p tmp_mix/tr_wsj-spk-bal/CH$ch/
+    awk -v ch=$ch '{print "tr_wsj-spk-bal/CH"ch"/"$1".wav"}' \
+      data/intv_CH${ch}_tr_wsj-spk-bal/segments > tmp_mix/tr_wsj-spk-bal/CH$ch/filelist.txt
   done
 
-  local/filelist_to_mixlist.py --write-stats True --target-mix-num 5000 tmp_mix/cv/CH02/filelist.txt \
+  # Create base mixture lists
+  local/filelist_to_mixlist.py --target-mix-num 5000 tmp_mix/cv/CH02/filelist.txt \
     mixes/mix_2_spk_CH02_cv.txt
-  local/filelist_to_mixlist.py --write-stats True --target-mix-num 3000 tmp_mix/tt/CH02/filelist.txt \
+  local/filelist_to_mixlist.py --target-mix-num 3000 tmp_mix/tt/CH02/filelist.txt \
     mixes/mix_2_spk_CH02_tt.txt
-  local/filelist_to_mixlist.py --write-stats True --target-mix-num 20000 tmp_mix/tr/CH02/filelist.txt \
+  local/filelist_to_mixlist.py --target-mix-num 20000 tmp_mix/tr/CH02/filelist.txt \
     mixes/mix_2_spk_CH02_tr.txt
 
-  for dset in tr tt cv; do
+  # Create 100,000-mixture training set mixture list
+  local/filelist_to_mixlist.py --target-mix-num 100000 tmp_mix/tr/CH02/filelist.txt \
+    mixes/mix_2_spk_CH02_tr_100k.txt
+
+  # Create WSJ0-speaker-balanced training set mixture list
+  local/filelist_to_mixlist.py --target-mix-num 20000 tmp_mix/tr_wsj-spk-bal/CH02/filelist.txt \
+    mixes/mix_2_spk_CH02_tr_wsj-spk-bal.txt
+
+  for dset in tr tt cv tr_100k tr_wsj-spk-bal; do
     sed -e 's/CH02/CH09/g' mixes/mix_2_spk_CH02_${dset}.txt > mixes/mix_2_spk_CH09_${dset}.txt
   done
 fi
