27936379918a332aa96677191fb78c5fe5f6ec63
Matthew Maciejewski <mmaciej2@jhu.edu>
2018-11-08 12:07:36 -0500

    reordered mic/dataset for mixtures

--- egs/chime5/single_speaker_generation/run_create_mix_lists.sh
+++ egs/chime5/single_speaker_generation/run_create_mix_lists.sh
@@ -17,15 +17,15 @@ set -e # exit on error
 
 if [ $stage -le 1 ]; then
   for dset in dev train; do
-    mkdir -p tmp_mix/U01/$dset
-    awk -v mic=U01 -v dset=$dset '{print mic"/"dset"/"$1".wav"}' \
-      data/${dset}_final/U01/segments > tmp_mix/U01/$dset/filelist.txt
+    mkdir -p tmp_mix/$dset/U01
+    awk -v mic=U01 -v dset=$dset '{print dset"/"mic"/"$1".wav"}' \
+      data/${dset}_final/U01/segments > tmp_mix/$dset/U01/filelist.txt
   done
 
   mkdir -p mixes
-  local/filelist_to_mixlist.py --target-mix-num 5000 tmp_mix/U01/dev/filelist.txt \
+  local/filelist_to_mixlist.py --target-mix-num 5000 tmp_mix/dev/U01/filelist.txt \
     mixes/mix_2_spk_U01_dev.txt
-  local/filelist_to_mixlist.py --target-mix-num 20000 tmp_mix/U01/train/filelist.txt \
+  local/filelist_to_mixlist.py --target-mix-num 20000 tmp_mix/train/U01/filelist.txt \
     mixes/mix_2_spk_U01_train.txt
 fi
 
@@ -38,10 +38,10 @@ if [ $stage -le 2 ]; then
       awk '{for(i=1; i<=NF; i++){spk=substr($i,9,3); mic=substr($i,5,3); if(spk=="P14" && substr($i,1,3)=="S05"){if(mic=="P15"){print $i}} else if(spk=="P54" && substr($i,1,3)=="S23"){if(mic=="P56"){print $i}} else {if(mic==spk){print $i}}}}' > close_talking_utterances.tmp
     paste -d' ' U01_utterances.tmp close_talking_utterances.tmp >> U01_to_close_talking_map.txt
 
-    sed -e "s/U01\/${dset}\///g;s/\.wav//g" mixes/mix_2_spk_U01_${dset}.txt |\
+    sed -e "s/${dset}\/U01\///g;s/\.wav//g" mixes/mix_2_spk_U01_${dset}.txt |\
       utils/apply_map.pl -f 1 U01_to_close_talking_map.txt |\
       utils/apply_map.pl -f 3 U01_to_close_talking_map.txt |\
-      awk -v dset=$dset '{print "close_talking/"dset"/"$1".wav "$2" close_talking/"dset"/"$3".wav "$4}' \
+      awk -v dset=$dset '{print dset"/close_talking/"$1".wav "$2" "dset"/close_talking/"$3".wav "$4}' \
       > mixes/mix_2_spk_close_talking_${dset}.txt
   done
 fi
